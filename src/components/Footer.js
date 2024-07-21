2806b416efcbd0803147f052dad091cc3c50165e
SanjayRavichandran-Mscss <117557388+SanjayRavichandran-Mscss@users.noreply.github.com>
2024-07-21 14:57:06 +0530

    UI using NextJS & Tailwind CSS

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-gray-500 text-center py-4 pb-26 md:pb-4">
+      Copyrigh &copy; sick <br/> All Rights Reserved 2024
+    </div>
+  )
+}
+
+export default Footer
